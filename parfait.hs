47960c6acb17e6ef72f2d596012a8dfdb0595832
Ophir LOJKINE <pere.jobs@gmail.com>
2015-03-21 19:26:14 +0100

    Nombre parfaits: implémentation alternative en 1 ligne

--- parfait.hs
+++ parfait.hs
@@ -2,6 +2,10 @@ f n 0 acc = acc
 f n a acc = f n (a-1) (if rem n a == 0 then acc + a else acc)
 
 sommeDiv n = f n (n-1) 0
+
+-- Alternative, sans utiliser la fonction récursive f :
+-- sommeDiv n = sum $ filter ((==0).(rem n)) [1..n-1]
+
 estParfait n = sommeDiv n == n
 
-main = print(filter estParfait [1..500])
+main = print(filter estParfait [1..1000])
